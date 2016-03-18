3497774c3ec752f53fcaecdff289ef8a60eb4b69
Ambroz <killerbee8@gmail.com>
2016-03-18 23:17:46 +0100

    drobne zmiany w ustawieniu okna adresy.php

--- scripts/otwieranie_okna.js
+++ scripts/otwieranie_okna.js
@@ -1,6 +1,6 @@
 var myWindow;
 				function funkcja_adresy()
 				{
-				var myWindow = window.open("../autoryzacja/zal_kont.php", "Adresy", "width=1000, height=800");<!-- Adres, nazwa, parametry-->
+				var myWindow = window.open("../autoryzacja/zal_kont.php", "Adresy", "width=1500, height=600, top=250, left=300");<!-- Adres, nazwa, parametry-->
 				<!-- myWindow.document.write("<p>This is 'MsgWindow'. I am 200px wide and 200px tall!</p>");
 				}
