da01cdbdfc42b4a8c4d75a73081a771b9a05b633
Bartosz Kustra <bart@webflow.com>
2016-04-26 23:37:14 +0200

    Add ajaxSubmit() function to actually submit form

--- scripts/tablica2.js
+++ scripts/tablica2.js
@@ -59,7 +59,7 @@ $(document).ready(function() {
       url: '../connects/insert_to_db.php',
       type: 'post'
     });
-    console.log('DEFUQ!');
+    $('#dodajStudenta').ajaxSubmit();
     table.ajax.reload();
   });
 
