2671fe171245201a2da32ff8dedf5aaea6462990
Bartosz Kustra <bart@webflow.com>
2016-04-26 22:34:40 +0200

    Modal - EMPTY

--- php/bramka.php
+++ php/bramka.php
@@ -20,54 +20,61 @@
 	<link rel="stylesheet" href="../css/style.css">
 
 
-
-
 </head>
 
 <body>
-<div id="container">
-	<div id="logo">
-<img src="../img/logo.jpg"/>
+	<div id="adresymodal-bg" class="adresymodal-bg" style="display: none;">
+    <div class="adresymodal-container">
+      <div class="w-clearfix">
+        <div class="adresymodal-close">Zamknij</div>
+      </div>
+      <div class="adresymodal-window"></div>
+    </div>
+  </div>
+
+	<div id="container">
+		<div id="logo">
+	<img src="../img/logo.jpg"/>
+		</div>
+	<br />
+	<?php
+	echo "<p>Witaj ".$_SESSION['user'].'! [<a href="logout.php">Wyloguj się!</a>]</p>';
+
+	?>
+
+	<br />
+		<div id="upperbutton">
+			<button  id="adresy" onclick="funkcja_adresy()"></button>
+		</div>
+		<!--<input type="text" id="kontakty" name="konatakty"/>-->
+
+		<div id="lewy">
+		<div id="charNum">160</div>
+
+	<form action="sms.php" method="post">
+		<input type="text" id="numer" name="numer"/>
+		<textarea type="text" name="wiadomosc" id="wiadomosc" onkeyup="countChar(this)" maxlength="160"></textarea><br />
+	  <input type="submit" value="Wyslij" name="wyslij" id="wyslij">
+	</form>
+
+	<br />
 	</div>
-<br />
-<?php
-echo "<p>Witaj ".$_SESSION['user'].'! [<a href="logout.php">Wyloguj się!</a>]</p>';
-
-?>
-
-<br />
-	<div id="upperbutton">
-		<button  id="adresy" onclick="funkcja_adresy()"></button>
+	<div id="prawy">
+	<?php
+	if (isset($_SESSION['poszlo'])) //sprawdza czy zmienna błąd jest ustawiona w sesji
+	{
+			echo $_SESSION['poszlo'];
+	}
+	?>
 	</div>
-	<!--<input type="text" id="kontakty" name="konatakty"/>-->
-
-	<div id="lewy">
-	<div id="charNum">160</div>
-
-<form action="sms.php" method="post">
-	<input type="text" id="numer" name="numer"/>
-	<textarea type="text" name="wiadomosc" id="wiadomosc" onkeyup="countChar(this)" maxlength="160"></textarea><br />
-  <input type="submit" value="Wyslij" name="wyslij" id="wyslij">
-</form>
-
-<br />
-</div>
-<div id="prawy">
-<?php
-if (isset($_SESSION['poszlo'])) //sprawdza czy zmienna błąd jest ustawiona w sesji
-{
-		echo $_SESSION['poszlo'];
-}
-?>
-</div>
 
 
 
 
 
-<script src="http://code.jquery.com/jquery-1.5.js"></script>
-<script type="text/javascript" src="../scripts/liczenie_znaki.js"></script>
-<script type="text/javascript" src="../scripts/otwieranie_okna.js"></script>
+	<script src="http://code.jquery.com/jquery-1.5.js"></script>
+	<script type="text/javascript" src="../scripts/liczenie_znaki.js"></script>
+	<script type="text/javascript" src="../scripts/otwieranie_okna.js"></script>
 
 
 </body>
