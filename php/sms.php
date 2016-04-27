574d9227c84cb06b04721f6c51591fb4d3bfabba
Ambroz <killerbee8@gmail.com>
2016-04-27 20:21:48 +0200

    Wersja wyświetlająca 2 numery

--- php/sms.php
+++ php/sms.php
@@ -1,29 +1,51 @@
-<?php
-  $numer = $_POST['numer']; //pobranie loginu wysłanego z indexu
-  $wiadomosc = $_POST['wiadomosc']; //pobranie hasła
-  $msgCenter = array();
+    <?php
+//session_start();
 
-  echo $numer;
-  echo $wiadomosc;
+$numer = $_POST['numer']; //pobranie loginu wysłanego z indexu
+$wiadomosc = $_POST['wiadomosc']; //pobranie hasła
+$msgCenter = array();
 
-  $numerArray = explode(",", $numer);
-  for($i = 0; $i < count($numerArray); $i++) {
-    array_push($msgCenter, array($numerArray[$i], $wiadomosc));
+// echo $numer;
+// echo $wiadomosc;
+
+$numerArray = explode(",", $numer);
+for($i = 0; $i < count($numerArray); $i++) {
+  array_push($msgCenter, array($numerArray[$i], $wiadomosc));
+}
+
+ fifomsgs($msgCenter);
+
+function fifomsgs($msg) {
+  if (count($msg) != 0) {
+    //sendMessage($msg[0][0], $msg[0][1]);
+    echo "__1___";
+    echo $msg[0][0];
+    echo $msg[0][1];
+    //sleep(5);
+    array_shift($msg);
+
+    // sendMessage($msg[0][0], $msg[0][1]);
+        //  echo $msgCenter;
+    echo "__2___";
+    echo $msg[0][0];
+    echo $msg[0][1];
+    // sendMessage($msg[0][0], $msg[0][1]);
+    fifomsgs($msgCenter);
+    // print_r($msgCenter);
+    // sendMessage($msg[0][0], $msg[0][1]);
+    echo "<br />";
   }
+  // print_r($msg);
 
-  fifomsgs($msgCenter);
+}
 
-  function fifomsgs($msg) {
-    if (count($msg) > 0) {
-      sendMessage($msg[0][0], $msg[0][1]);
-      array_shift($msgCenter);
-      fifomsgs($msgCenter);
-    }
-  }
+// [wiadomosc nr 1, wiadomosc nr 2]
 
-  function sendMessage($sendToNumer, $sendToMessage) {
-    // tutaj cala logika od wysylania wiadomosci
-    include "php_serial.class.php";
+//664861998,500072122
+
+function sendMessage($sendToNumer, $sendToMessage) {
+   // tutaj cala logika od wysylania wiadomosci
+   include "php_serial.class.php";
     $serial = new phpSerial();
     $serial->deviceSet("COM1");
     $serial->confBaudRate(115200);
@@ -31,27 +53,30 @@
     //$serial->confCharacterLength(8);
     //$serial->confStopBits(1);
     //$serial->confFlowControl("none");
-
-    // Then we need to open it
+    //echo "tu jestem, po konfiguracji";
+     // Then we need to open it
     $serial->confCharacterLength(8);
     $serial->deviceOpen('w+');
     $serial->sendMessage("at+cmgf=1");
-    //$serial->sendMessage("AT+CSMP=\"17,100,0,240\"\n");
+   //$serial->sendMessage("AT+CSMP=\"17,100,0,240\"\n");
     $serial->sendMessage(chr(13));
-    sleep(1);
-    //$serial->sendMessage(chr(13));
-    $serial->sendMessage("at+cmgs=\"664861998\"");
-    //  $serial->sendMessage("at+cmgs=\"".$msgCenter[0][0]."\"");
-    $serial->sendMessage(chr(13));
-    //$serial->sendMessage(chr(10));
-    sleep(2);
-    $serial->sendMessage("BlaBla");
-    //  $serial->sendMessage($msgCenter[0][1]);
-    $serial->sendMessage(chr(26));
-    //  $read = $serial->readPort(1);
-    //  echo "       OdpowiedĹş:        ";
-    //  echo "Trzeci ".$read;
-    //$serial->sendMessage("ATE1\n");
-    $serial->deviceClose();
-  }
+   //sleep(1);
+   //$serial->sendMessage(chr(13));
+   //$serial->sendMessage("at+cmgs=\"664861998\"");
+   $serial->sendMessage("at+cmgs=\"".$sendToNumer."\"");
+   $serial->sendMessage(chr(13));
+   //$serial->sendMessage(chr(10));
+   //sleep(2);
+   //$serial->sendMessage("BlaBla");
+   $serial->sendMessage($sendToMessage);
+   $serial->sendMessage(chr(26));
+
+     //$read = $serial->readPort(1);
+   //  echo "       OdpowiedĹş:        ";
+     //echo "Trzeci ".$read;
+   //$serial->sendMessage("ATE1\n");
+   $serial->deviceClose();
+   sleep(5);
+
+}
 ?>
